ccbc50cdc49912c1ce0721ab4dc80d02ebb34422
chrisfranz <chrisfranz@mac.com>
2018-12-26 14:15:31 -0800

    initial commit, connecting Left and Center, adding file write/save

---
+++ src/utils/componentRender.util.js
@@ -0,0 +1,28 @@
+const componentRender = () => {
+  return `
+    <template>
+    
+    </template>
+  
+    <script>
+      export default {
+        name: '${ /* formatted component name */ }',
+        components: {
+
+        },
+        methods: {
+
+        },
+        computed: {
+
+        }
+      }
+    </script>
+
+    <style>
+    
+    </style>
+  `;
+};
+
+export default componentRender;
