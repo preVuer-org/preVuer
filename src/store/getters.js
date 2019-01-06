c8615f59e224cd99d1d75dfd57d75ce0962ddf4c
chrisfranz <chrisfranz@mac.com>
2019-01-05 16:45:39 -0800

    tree algorithm allows for deep nesting of components

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,9 +1,7 @@
-// import formatNestableItems from '../utils/formatNestableItems.util';
-// import formatNestableItemsDeep from '../utils/formatNestableDeep.util'
-import formatNestableItemsJim from '../utils/formatNestableJim';
+import unflatten from '../utils/unflatten.util'
+
 export default {
   getComponents: state => state.components,
   getCurrentText: state => state.currentText,
-  // getNestableItems: state => formatNestableItems(state.components)
-  getNestableItems: state => formatNestableItemsJim(state.components)
+  getNestableItems: state => unflatten(state.components)
 };
