6e3812bf28004c90442e951987ff001076d95f5a
chrisfranz <chrisfranz@mac.com>
2019-01-09 15:51:14 -0800

    removed @ statements from main.js

--- src/main.js
+++ src/main.js
@@ -7,16 +7,6 @@ import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/black-green-dark.css';
 import "vue-material-design-icons/styles.css";
 
-@import "~vue-material/dist/theme/engine"; // Import the theme engine
-
-@include md-register-theme("default", (
-  primary: md-get-palette-color(blue, A200), // The primary color of your application
-  accent: md-get-palette-color(red, A200), // The accent or secondary color
-  theme: dark // This can be dark or light
-));
-
-@import "~vue-material/dist/components/MdField/theme";
-
 import App from './App.vue';
 import store from './store/store';
 
