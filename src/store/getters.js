5eee66f30966257c0d6ad3f54bfd3cbc0d22e686
Jim Yoon <jimyoon90@gmail.com>
2019-01-14 21:09:59 -0800

    removed all files and folders, but still working version

--- src/store/getters.js
+++
@@ -1,8 +0,0 @@
-import unflatten from '../utils/unflatten.util';
-
-export default {
-  getComponents: state => state.components,
-  getCurrentText: state => state.currentText,
-  getImagePath: state => state.imagePath,
-  getNestableItems: state => unflatten(state.components),
-};
